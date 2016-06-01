829acd05854dd259156a85bd16411643ba5d9a82
marinacor1 <marinaspam1234@gmail.com>
2016-06-01 12:44:37 -0600

    Serializers for all models created

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,4 @@
 class Transaction < ActiveRecord::Base
   belongs_to :invoice
 
-  def as_json(options)
-    super({only: [:id, :credit_card_number, :credit_card_expiration_date, :result, :invoice_id,]}.merge(options))
-  end
-
 end
