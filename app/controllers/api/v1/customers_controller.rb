22774ba268b272059fb08293b226e1d6828a243d
marinacor1 <marinaspam1234@gmail.com>
2016-05-31 20:45:47 -0600

    Customer show page show information for one customer

---
+++ app/controllers/api/v1/customers_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class CustomersController < Api::ApiController
+      respond_to :json
+
+      def index
+        respond_with Customer.all
+      end
+
+      def show
+        respond_with Customer.where(customer_params)
+      end
+
+      private
+
+      def customer_params
+        params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
+      end
+    end
+  end
+end
