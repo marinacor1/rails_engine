f7d4499b79d6e557b0649483d77fbf98e4926c65
marinacor1 <marinaspam1234@gmail.com>
2016-06-01 12:51:11 -0600

    unit price in decimals now

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,8 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :unit_price
+  attribute :convert_to_dollars, key: :dollar
+
+  def convert_to_dollars
+    (object.unit_price.to_d)/100
+  end
 end
