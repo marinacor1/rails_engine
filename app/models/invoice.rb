3edc2ddb41cb663a329139a6ec979c06e4df5cf2
marinacor1 <marinaspam1234@gmail.com>
2016-05-31 21:58:14 -0600

    Invoice routes all work

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,9 @@
 class Invoice < ActiveRecord::Base
   belongs_to :customer
   belongs_to :merchant
+
+  def as_json(options)
+    super({only: [:id, :customer_id, :merchant_id, :status]}.merge(options))
+  end
+
 end
