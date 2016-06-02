6d8ad7de84444698a656e0d4bc91f60076113c7e
marinacor1 <marinaspam1234@gmail.com>
2016-06-01 20:25:57 -0600

    merchant index test passing

--- test/controllers/api/v1/merchants_controller_test.rb
+++ test/controllers/api/v1/merchants_controller_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 class Api::V1::MerchantsControllerTest < ActionController::TestCase
   test "it shows all merchant items" do
     get :index, format: :json
+    parsed_json = JSON.parse(response.body)
 
     assert_response :success
+    assert_equal 2, parsed_json.count
   end
 end
