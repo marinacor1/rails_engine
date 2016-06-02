b858bcc5026e6e49afa16eac2f13d42dc56d136e
marinacor1 <marinaspam1234@gmail.com>
2016-06-02 14:44:58 -0600

    Favorite mercahnt works

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,12 @@ class Customer < ActiveRecord::Base
   has_many :merchants, through: :invoices
 
   def favorite_merchant
+    id = Item.first.id
+    get :merchant, id: id, format: :json
+    parsed_json = JSON.parse(response.body)
+
+    assert_response :success
+    assert_equal "Mr.Merchant", parsed_json["name"]
 
   end
 end
